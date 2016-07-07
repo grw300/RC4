4f2661f2b3b2b7f7edd27f4b229297da94a91f4d
Greg Williams <grw300@gmail.com>
2016-07-07 13:37:21 -0500

    Key Initialization cleanup and new driver steps to show period

--- RC4.Driver/Program.cs
+++ RC4.Driver/Program.cs
@@ -1,34 +1,47 @@
 ﻿using System;
-using RC4;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RC4.Driver
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			byte[] key = { 1, 2, 3, 4, 5 };
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
 
-			var dataText = "Hello Greg!";
-			var data = Encoding.Unicode.GetBytes(dataText);
+            byte[] key = { 1, 2, 3, 4, 5 };
 
-			var cipher = RC4.Encrypt(key, data);
-			var cipherText = Encoding.Unicode.GetString(cipher);
+            var dataText = @"Hello Greg!";
+            var data = Encoding.Unicode.GetBytes(dataText);
 
-			var retrieved = RC4.Decrypt(key, cipher);
-			var retrievedText = Encoding.Unicode.GetString(retrieved);
+            var cipher = RC4.Encrypt(key, data);
+            var cipherText = Encoding.Unicode.GetString(cipher);
 
-			Console.WriteLine($"Data text     : {dataText}");
-			Console.WriteLine($"Cipher text   : {cipherText}");
-			Console.WriteLine($"Retrieved text: {retrievedText}");
+            var retrieved = RC4.Decrypt(key, cipher);
+            var retrievedText = Encoding.Unicode.GetString(retrieved);
 
-			byte[] s = RC4.KeyInitialization(key);
+            Console.WriteLine($"Data text     : {dataText}");
+            Console.WriteLine($"Cipher text   : {cipherText}");
+            Console.WriteLine($"Cipher bytes   : {PrintByteArray(cipher)}");
+            Console.WriteLine($"Retrieved text: {retrievedText}");
 
-			int index = Array.IndexOf(s, s[0], Array.IndexOf(s, s[0]) + 1);
+            Console.WriteLine($"Period of first pseudo-random byte: {RC4.GetKeyInitializationPeriod(key, 1)}");
+            Console.WriteLine($"Period of second pseudo-random byte: {RC4.GetKeyInitializationPeriod(key, 2)}");
 
-			Console.WriteLine($"Period: {index}");
-		}
-	}
+            Console.ReadKey();
+        }
+        public static string PrintByteArray(byte[] bytes)
+        {
+            var sb = new StringBuilder("new byte[] { ");
+            foreach (var b in bytes)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(" }");
+            return sb.ToString();
+        }
+    }
+
 }
--- RC4/RC4.cs
+++ RC4/RC4.cs
@@ -4,67 +4,89 @@ using System.Linq;
 
 namespace RC4
 {
-	public static class RC4
-	{
-		public static byte[] KeyInitialization(byte[] key)
-		{
-			byte[] s = Enumerable.Range(0, 256)
-			                     .Select(b => (byte)b)
-								 .ToArray();
+    public static class RC4
+    {
+        public static byte[] KeyInitialization(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256)
+                                 .Select(b => (byte)b)
+                                 .ToArray();
 
-			var t = new byte[256];
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                j = (j + s[i] + key[i % key.Length]) % 256;
+                Swap(s, i, j);
+            }
 
-			for (int i = 0; i < 256; i++)
-			{
-				s[i] = (byte)i;
-				t[i] = key[i % key.Length];
-			}
+            return s;
+        }
 
-			for (int i = 0, j = 0; i < 256; i++)
-			{
-				j = (j + s[i] + t[i]) % 256;
-				Swap(s, i, j);
-			}
+        public static int GetKeyInitializationPeriod(byte[] key, int position)
+        {
+            byte[] s = Enumerable.Range(0, 1000000)
+                                 .Select(b => (byte)b)
+                                 .ToArray();
+            int i = 0, j = 0;
 
-			return s;
-		}
+            bool startCheck = false;
 
-		public static byte[] Decrypt(byte[] key, byte[] cipher)
-		{
-			return Encrypt(key, cipher);
-		}
+            while (true)
+            {
+                j = (j + s[i] + key[i % key.Length]) % 256;
+                Swap(s, i, j);
 
-		public static byte[] Encrypt(byte[] key, byte[] data)
-		{
-			byte[] s = KeyInitialization(key);
+                if (startCheck)
+                {
+                    if (s[i] == s[position])
+                        return i - position;
+                }
 
-			byte[] cipher = new byte[data.Length];
+                i++;
 
-			int i = 0, j = 0;
+                if (i > position)
+                    startCheck = true;
 
-			for (int l = 0; l < data.Length; l++)
-			{
-				i = (i + 1) % 256;
-				j = (j + s[i]) % 256;
+                if (i >= 1000000)
+                    return -1;
+            }
+        }
 
-				Swap(s, i, j);
+        public static byte[] Decrypt(byte[] key, byte[] cipher)
+        {
+            return Encrypt(key, cipher);
+        }
 
-				var t = (s[i] + s[j]) % 256;
+        public static byte[] Encrypt(byte[] key, byte[] data)
+        {
+            byte[] s = KeyInitialization(key);
 
-				var k = s[t];
+            byte[] cipher = new byte[data.Length];
 
-				cipher[l] = (byte)(data[l] ^ k);
-			}
+            int i = 0, j = 0;
 
-			return cipher;
-		}
+            for (int l = 0; l < data.Length; l++)
+            {
+                i = (i + 1) % 256;
+                j = (j + s[i]) % 256;
 
-		private static void Swap(byte[] s, int i, int j)
-		{
-			byte c = s[i];
-			s[i] = s[j];
-			s[j] = c;
-		}
-}
+                Swap(s, i, j);
+
+                var t = (s[i] + s[j]) % 256;
+
+                var k = s[t];
+
+                cipher[l] = (byte)(data[l] ^ k);
+            }
+
+            return cipher;
+        }
+
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+        }
+    }
 }
 
